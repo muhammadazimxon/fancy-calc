29c77359169c4133dd04de4b5b22a44c04804301
Zboot007 <92007138+muhammadazimxon@users.noreply.github.com>
2025-05-01 13:22:18 +0500

    Solution

--- FancyCalc/Operations.cs
+++ FancyCalc/Operations.cs
@@ -6,21 +6,10 @@ namespace FancyCalc
     {
         public static int Plus(int x, int y) => x + y;
 
-        public static int Minus(int x, int y)
-        {
-            return x - y;
-        }
+        public static int Minus(int x, int y) => x - y;
 
-        public static int Multiply(int x, int y)
-        {
-            int result = x * y;
-            return result;
-        }
+        public static int Multiply(int x, int y) => x * y;
 
-        public static int Sum(int x1, int x2, int x3)
-        {
-            int sum = x1 + x2 + x3;
-            return sum;
-        }
+        public static int Sum(int x1, int x2, int x3) => x1 + x2 + x3;
     }
 }
