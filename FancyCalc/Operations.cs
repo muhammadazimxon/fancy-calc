5293a8c476734c5fbe9a42dbb71d41098d4e706c
Zboot007 <92007138+muhammadazimxon@users.noreply.github.com>
2025-05-01 13:15:15 +0500

    Solution

--- FancyCalc/Operations.cs
+++ FancyCalc/Operations.cs
@@ -11,23 +11,18 @@ namespace FancyCalc
 
         public static int Minus(int x, int y)
         {
-            // TODO #2. Replace "x + y - x" in the next line with "x - y".
             return x - y;
         }
 
         public static int Multiply(int x, int y)
         {
-            // TODO #3. Add a breakpoint to the next line, run the unit test in the debug mode, and inspect the result during the program execution.
-            int result = x - y;
+            int result = x * y;
             return result;
         }
 
         public static int Sum(int x1, int x2, int x3)
         {
-            // TODO #4. Add a breakpoint to the next line, run the unit test in the debug mode, and trace the program execution.
-            int sum1 = x1 + x2;
-            int sum2 = x2 + x3;
-            int sum = sum1 + sum2;
+            int sum = x1 + x2 + x3;
             return sum;
         }
     }
